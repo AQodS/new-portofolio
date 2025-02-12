a5e8a237b3d055b8e7a4b80b512aa5402327084a
AQodS <qoddri@gmail.com>
2025-02-13 02:22:23 +0700

    feat: Review

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import Review from "./components/Review";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <About />
         <Skills />
         <Work />
+        <Review />
       </main>
     </>
   );
